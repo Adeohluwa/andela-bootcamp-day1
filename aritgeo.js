126a94f13120380f42a5ff8ebd9b3383581ab774
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-01 08:49:06 +0100

    add proper indentation to aritgeo.js

--- aritgeo.js
+++ aritgeo.js
@@ -4,47 +4,49 @@
 
 //function returns true if array is an A.P
 const arithmetic = (array)=>{
-	commonDiff = array[1] - array[0];
-	for (let i = 0; i < array.length -1; i++) {
-		if (commonDiff !== (array[i+1] - array[i])) {
-			return false;
-		}
-	}
+    commonDiff = array[1] - array[0];
+    for (let i = 0; i < array.length -1; i++) {
+        if (commonDiff !== (array[i+1] - array[i])) {
+            return false;
+        }
+    }
 	return true;	
 
 }
 
 //function returns true if array is a G.P
 const geometric = (array)=>{
-	commonRatio = array[1] / array[0];
-	for (let i = 0; i < array.length - 1; i++){
-		if (commonRatio !== (array[i+1] / array[i])) {
-			return false; 
-		}
-	}
+    commonRatio = array[1] / array[0];
+    for (let i = 0; i < array.length - 1; i++){
+        if (commonRatio !== (array[i+1] / array[i])) {
+            return false; 
+        }
+    }
 	return true;
 }
 
 //function checks if its an array and is empty
 const emptyArray = (array)=>{
-	if ((array.constructor === Array ) && (array.length < 1 )){
-		return true;
-	}
-	return false;
+    if ((array.constructor === Array ) && (array.length < 1 )){
+        return true;
+    }
+    return false;
 }
 
 
 
-module.exports = {aritGeo: (array)=>{
-					if (emptyArray(array)) {
-							return 0;		
-						}
-					if (arithmetic(array)) {
-							return "arithmetic";
-						}
-					if (geometric(array)) {
-							return "geometric";
-						}
-					//return -1 for neither
-					return -1;
-					}}
+module.exports = {
+    aritGeo: (array)=>{
+        if (emptyArray(array)) {
+            return 0;		
+        }
+        if (arithmetic(array)) {
+            return "arithmetic";
+        }
+        if (geometric(array)) {
+            return "geometric";
+        }
+        //return -1 for neither
+        return -1; 
+    }
+}
